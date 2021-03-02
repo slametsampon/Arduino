56428c2289bd30896d6d2db60b1b8610c5f0d444
sampon <slamet@pon.co.id>
2021-03-02 11:19:31 +0700

    testing pbLed

--- Libraries/pbLed/pbAMR.h
+++ Libraries/pbLed/pbAMR.h
@@ -10,23 +10,25 @@
 #ifndef pbAMR_h
 #define pbAMR_h
 
-const char PB_READY = 0;
-const char PB_AUTO = 1;
-const char PB_MANUAL = 2;
-const char PB_MANUAL_ON = 3;
-const char PB_RESET = 4;
-const unsigned long DEBOUNCE_PB = 150;//milli second
+const char STATUS_READY = 0;
+const char STATUS_AUTO = 1;
+const char STATUS_MANUAL = 2;
+const char STATUS_MANUAL_ON = 3;
+const char STATUS_RESET = 4;
+//const unsigned long DEBOUNCE_PB = 150;//milli second
 
 #include "Arduino.h"
 #include "switchExt.h"
+
     class PbAMR{
         public:
-            PbAMR(switchExt*, switchExt*, switchExt*);
-            char getCmd();
+            PbAMR(SwitchExt*, SwitchExt*, SwitchExt*);
+            char getCmd(unsigned long);
             void info();
         private:
-            switchExt *_pbAuto, *_pbManual, *_pbReset;
+            SwitchExt *_pbAuto, *_pbManual, *_pbReset;
             char    _prevCmd;
+            bool  _isInfo;//one shoot info
     };
 
 #endif
